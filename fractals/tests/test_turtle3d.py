67595f094e0de2782c7c27f5d4d41d27b0a31539
Austin Gill <notgnoshi@gmail.com>
2019-04-05 09:30:44 -0600

    Use better method for asserting Numpy array equality

--- fractals/tests/test_turtle3d.py
+++ fractals/tests/test_turtle3d.py
@@ -7,6 +7,9 @@ from fractals.graphics import Turtle3D
 
 
 class TestTurtle3D(unittest.TestCase):
+    def assertArrayAlmostEqual(self, actual, desired):
+        return np.testing.assert_allclose(actual, desired)
+
     def setUp(self):
         self.start_position = np.array([0, 0, 0])
         self.start_orientation = Quaternion(axis=[0, 0, 1], angle=0)
@@ -37,14 +40,14 @@ class TestTurtle3D(unittest.TestCase):
 
         self.assertTrue(isinstance(t.position, np.ndarray))
         self.assertTrue(isinstance(t.orientation, Quaternion))
-        self.assertTrue(np.all(t.position == self.start_position))
+        self.assertArrayAlmostEqual(t.position, self.start_position)
         self.assertEqual(t.orientation, self.start_orientation)
 
         t = Turtle3D(step=self.step, angle=self.angle, position=(1, 1, 1))
 
         self.assertTrue(isinstance(t.position, np.ndarray))
         self.assertTrue(isinstance(t.orientation, Quaternion))
-        self.assertTrue(np.all(t.position == np.array([1, 1, 1])))
+        self.assertArrayAlmostEqual(t.position, np.array([1, 1, 1]))
         self.assertEqual(t.orientation, self.start_orientation)
 
         t = Turtle3D(
@@ -53,50 +56,50 @@ class TestTurtle3D(unittest.TestCase):
 
         self.assertTrue(isinstance(t.position, np.ndarray))
         self.assertTrue(isinstance(t.orientation, Quaternion))
-        self.assertTrue(np.all(t.position == self.start_position))
+        self.assertArrayAlmostEqual(t.position, self.start_position)
         self.assertEqual(t.orientation, Quaternion(axis=[1, 0, 0], angle=np.pi))
 
     def test_forward(self):
         t = Turtle3D(step=self.step, angle=self.angle)
 
-        self.assertTrue(np.all(t.position == np.array([0, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 0]))
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([0, 0, 1])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 1]))
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([0, 0, 2])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 2]))
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([0, 0, 3])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 3]))
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([0, 0, 4])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 4]))
 
     def test_pitch(self):
         t = Turtle3D(step=self.step, angle=self.angle)
-        self.assertTrue(np.all(t.position == np.array([0, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 0]))
         # The starting orientation should be the turtle facing straight up with
         # its back towards the x axis.
         self.assertEqual(t.orientation, Quaternion(axis=[0, 0, 1], angle=0))
 
         t.pitch()
         # Should not move.
-        self.assertTrue(np.all(t.position == np.array([0, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([0, 0, 0]))
         # Should be oriented in the x direction.
         self.assertEqual(t.orientation, Quaternion(axis=[1, 0, 0], angle=np.pi / 2))
 
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([1, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([1, 0, 0]))
 
         t.pitch(down=True)
-        self.assertTrue(np.all(t.position == np.array([1, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([1, 0, 0]))
         self.assertEqual(t.orientation, Quaternion(axis=[0, 0, 1], angle=0))
 
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([1, 0, 1])))
+        self.assertArrayAlmostEqual(t.position, np.array([1, 0, 1]))
 
         t = Turtle3D(step=self.step, angle=self.angle)
         t.pitch(down=True)
         self.assertEqual(t.orientation, Quaternion(axis=[-1, 0, 0], angle=0))
         t.forward()
-        self.assertTrue(np.all(t.position == np.array([-1, 0, 0])))
+        self.assertArrayAlmostEqual(t.position, np.array([-1, 0, 0]))
 
     @unittest.skip("TODO: Write this test.")
     def test_yaw(self):
