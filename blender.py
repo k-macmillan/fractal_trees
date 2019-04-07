dbedc11e787bf89af4001fe32f9b6153ea466b6e
Austin Gill <notgnoshi@gmail.com>
2019-04-07 17:25:22 -0600

    Move adding the objects to the scene to the graphics class

--- blender.py
+++ blender.py
@@ -3,97 +3,8 @@ import itertools
 import json
 import sys
 
-import bpy
 from fractals.grammar import Grammar
 from fractals.graphics import Graphics
-from mathutils import Vector
-
-
-def draw_all(cylinders):
-    """Draw the given cylinders.
-
-    Use a template cylinder for each length to avoid a scene update for each cylinder.
-
-    c.f. https://blender.stackexchange.com/questions/7358/python-performance-with-blender-operators
-
-    :param cylinders: A dict of (length, [{cyl}, ...]) pairs.
-    """
-    objs = []
-    count = 1
-
-    c_count = sum(len(clist) for clist in cylinders.values())
-
-    # The cylinders dict has form {length: [cylinders]}
-    for length, cylinder_list in cylinders.items():
-        # Generate ONE cylinder to duplicate in the appropriate position.
-        template_dict = cylinder_list[0]
-
-        v = Vector(tuple(c1 - c2 for c1, c2 in zip(template_dict["from"], template_dict["to"])))
-
-        # The primitive_cylinder_add forces a scene update.
-        bpy.ops.mesh.primitive_cylinder_add(
-            radius=template_dict["radius"], depth=v.magnitude, location=(10.0, 10.0, 10.0)
-        )
-        if template_dict["material"] == "Leaf":
-            mat = bpy.data.materials.new("material_leaf")
-            mat.diffuse_color = (0.0, 102 / 255, 0.0)
-        elif template_dict["material"] == "Branch":
-            mat = bpy.data.materials.new("material_branch")
-            mat.diffuse_color = (51 / 255, 26 / 255, 0.0)
-
-        template = bpy.context.active_object
-        template.name = "template_" + str(length)
-        # print("Template name: ", template.name)
-        template.active_material = mat
-        template.rotation_mode = "QUATERNION"
-        template.rotation_quaternion = (1, 0, 0, 0)
-
-        # Copy it for the number of items in the dictionary list
-        for cylinder in cylinder_list:
-            duplicate = template.copy()
-            # also duplicate mesh, remove for linked duplicate
-            duplicate.data = duplicate.data.copy()
-
-            center = Vector(
-                tuple((c1 + c2) / 2 for c1, c2 in zip(cylinder["from"], cylinder["to"]))
-            )
-            v = Vector(tuple(c1 - c2 for c1, c2 in zip(cylinder["from"], cylinder["to"])))
-            u = Vector((0, 0, v.magnitude))
-            q = u.rotation_difference(v)
-            duplicate.location = center
-            duplicate.rotation_quaternion = (q.w, q.x, q.y, q.z)
-            objs.append(duplicate)
-
-            print("\rprogress: {}% ({})".format(100 * count // c_count, count), end="")
-            count += 1
-
-            # Every so often, add the objects to the scene and join them together.
-            if count % 200 == 0:
-                bpy.ops.object.select_all(action="DESELECT")
-                # Add the objects to the scene and join them together.
-                for obj in objs:
-                    bpy.context.scene.objects.link(obj)
-                    obj.select = True
-                bpy.context.scene.objects.active = objs[0]
-                bpy.ops.object.join()
-                bpy.ops.object.select_all(action="DESELECT")
-                objs.clear()
-
-        # Delete the template cylinder.
-        bpy.ops.object.delete()
-
-    print("\nLinking remaining objects.")
-    # Add any remaining objects to the scene.
-    for obj in objs:
-        bpy.context.scene.objects.link(obj)
-
-    # Join the objects together, update the scene, and smooth the cylinders.
-    bpy.context.scene.objects.active = objs[0]
-    bpy.ops.object.select_all(action="SELECT")
-    bpy.ops.object.join()
-    bpy.context.scene.update()
-    bpy.ops.object.shade_smooth()
-    bpy.ops.object.select_all(action="DESELECT")
 
 
 def parse_args(argv):
@@ -137,7 +48,7 @@ def main(args):
         proportion=config["proportion"],
     )
     print("Drawing the cylinders.")
-    cylinders = graphics.draw(lstring)
+    cylinders = graphics.compute(lstring)
     c_count = sum(len(clist) for clist in cylinders.values())
 
     print(
@@ -151,12 +62,7 @@ def main(args):
     print("Adding {} cylinders to Blender scene.".format(c_count))
 
     # TODO: It's possible to combine objects from multiple blender files. Split up cylinders on large fractals.
-    bpy.ops.wm.read_factory_settings(use_empty=True)
-
-    draw_all(cylinders)
-
-    print("\nSaving scene to '" + basename + ".blend'")
-    bpy.ops.wm.save_mainfile(filepath=basename + ".blend")
+    graphics.draw(cylinders, basename + ".blend")
 
 
 if __name__ == "__main__":
--- fractals/graphics/graphics.py
+++ fractals/graphics/graphics.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from functools import partial
 
 import bpy
+from mathutils import Vector
 
 from .turtle import Turtle
 
@@ -74,7 +75,7 @@ class Graphics:
             "]": self.turtle.pop,
         }
 
-    def draw(self, commands):
+    def compute(self, commands):
         """Generate the 3D cylinders from the given graphics commands.
 
         :param commands: A string of successive graphics commands.
@@ -124,3 +125,95 @@ class Graphics:
         if data:
             with open(path + ".json", "w") as outfile:
                 json.dump(data, outfile)
+
+    @staticmethod
+    def draw(cylinders, filename):
+        """Draw the given cylinders.
+
+        Use a template cylinder for each length to avoid a scene update for each cylinder.
+
+        c.f. https://blender.stackexchange.com/questions/7358/python-performance-with-blender-operators
+
+        :param cylinders: A dict of (length, [{cyl}, ...]) pairs.
+        """
+        bpy.ops.wm.read_factory_settings(use_empty=True)
+        objs = []
+        count = 1
+
+        c_count = sum(len(clist) for clist in cylinders.values())
+
+        # The cylinders dict has form {length: [cylinders]}
+        for length, cylinder_list in cylinders.items():
+            # Generate ONE cylinder to duplicate in the appropriate position.
+            template_dict = cylinder_list[0]
+
+            v = Vector(tuple(c1 - c2 for c1, c2 in zip(template_dict["from"], template_dict["to"])))
+
+            # The primitive_cylinder_add forces a scene update.
+            bpy.ops.mesh.primitive_cylinder_add(
+                radius=template_dict["radius"], depth=v.magnitude, location=(10.0, 10.0, 10.0)
+            )
+            if template_dict["material"] == "Leaf":
+                mat = bpy.data.materials.new("material_leaf")
+                mat.diffuse_color = (0.0, 102 / 255, 0.0)
+            elif template_dict["material"] == "Branch":
+                mat = bpy.data.materials.new("material_branch")
+                mat.diffuse_color = (51 / 255, 26 / 255, 0.0)
+
+            template = bpy.context.active_object
+            template.name = "template_" + str(length)
+            # print("Template name: ", template.name)
+            template.active_material = mat
+            template.rotation_mode = "QUATERNION"
+            template.rotation_quaternion = (1, 0, 0, 0)
+
+            # Copy it for the number of items in the dictionary list
+            for cylinder in cylinder_list:
+                duplicate = template.copy()
+                # also duplicate mesh, remove for linked duplicate
+                duplicate.data = duplicate.data.copy()
+
+                center = Vector(
+                    tuple((c1 + c2) / 2 for c1, c2 in zip(cylinder["from"], cylinder["to"]))
+                )
+                v = Vector(tuple(c1 - c2 for c1, c2 in zip(cylinder["from"], cylinder["to"])))
+                u = Vector((0, 0, v.magnitude))
+                q = u.rotation_difference(v)
+                duplicate.location = center
+                duplicate.rotation_quaternion = (q.w, q.x, q.y, q.z)
+                objs.append(duplicate)
+
+                print("\rprogress: {}% ({})".format(100 * count // c_count, count), end="")
+                count += 1
+
+                # Every so often, add the objects to the scene and join them together.
+                if count % 200 == 0:
+                    bpy.ops.object.select_all(action="DESELECT")
+                    # Add the objects to the scene and join them together.
+                    for obj in objs:
+                        bpy.context.scene.objects.link(obj)
+                        obj.select = True
+                    bpy.context.scene.objects.active = objs[0]
+                    bpy.ops.object.join()
+                    bpy.ops.object.select_all(action="DESELECT")
+                    objs.clear()
+
+            # Delete the template cylinder.
+            template.select = True
+            bpy.ops.object.delete()
+
+        print("\nLinking remaining objects.")
+        # Add any remaining objects to the scene.
+        for obj in objs:
+            bpy.context.scene.objects.link(obj)
+
+        # Join the objects together, update the scene, and smooth the cylinders.
+        bpy.context.scene.objects.active = objs[0]
+        bpy.ops.object.select_all(action="SELECT")
+        bpy.ops.object.join()
+        bpy.context.scene.update()
+        bpy.ops.object.shade_smooth()
+        bpy.ops.object.select_all(action="DESELECT")
+
+        print("\nSaving scene to '" + filename + "'")
+        bpy.ops.wm.save_mainfile(filepath=filename)
