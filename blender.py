cfc3449671ce8eb17161fb285500294fef057655
Kyle MacMillan <macattackftw@gmail.com>
2019-04-07 02:59:36 -0600

    Modified data structures to allow for templatized copies. Speedup is significant.

--- blender.py
+++ blender.py
@@ -9,6 +9,66 @@ from fractals.graphics import Graphics
 from mathutils import Vector
 
 
+def draw_all(cylinders):
+    """https://blender.stackexchange.com/questions/7358/python-performance-with-blender-operators"""
+    sce = bpy.context.scene
+    obs = []
+
+    # cylinders are in the form [{"length": [cylinders]}]
+    for clist in cylinders:
+        # Generate ONE cylinder
+        clist = next(iter(clist.values()))
+        template = clist[0]
+
+        v = Vector(tuple(c1 - c2 for c1, c2 in zip(template["from"], template["to"])))
+        u = Vector((0, 0, v.magnitude))
+        q = u.rotation_difference(v)
+        
+        bpy.ops.mesh.primitive_cylinder_add(
+            radius=template["radius"],
+            depth=v.magnitude,
+            location=(0.0, 0.0, 0.0))
+        if template["material"] == "Leaf":
+            mat = bpy.data.materials.new("material_leaf")
+            mat.diffuse_color = (0.0, 102 / 255, 0.0)
+        elif template["material"] == "Branch":
+            mat = bpy.data.materials.new("material_leaf")
+            mat.diffuse_color = (51 / 255, 26 / 255, 0.0)
+
+        ob = bpy.context.active_object
+        ob.active_material = mat
+        ob.rotation_mode = "QUATERNION"
+        ob.rotation_quaternion = (1, 0, 0, 0)
+
+        # Copy it for the number of items in the dictionary list
+        for cylinder in clist:
+            duplicate = ob.copy()
+            duplicate.data = duplicate.data.copy() # also duplicate mesh, remove for linked duplicate
+
+            c = tuple((c1 + c2) / 2 for c1, c2 in zip(cylinder["from"], cylinder["to"]))
+            v = Vector(tuple(c1 - c2 for c1, c2 in zip(cylinder["from"], cylinder["to"])))
+            u = Vector((0, 0, v.magnitude))
+            q = u.rotation_difference(v)
+            # rot = cylinder['rotation'].to_quaternion()
+            # print(rot)
+            # print('x, y, z: {}, {}, {}'.format(c[0], c[1], c[2]))
+            duplicate.location = Vector((c[0], c[1], c[2]))
+            # duplicate.rotation_quaternion = rot
+            duplicate.rotation_quaternion = (q.w, q.x, q.y, q.z)
+            obs.append(duplicate)
+
+        
+
+        # Cleanup
+        bpy.ops.object.delete()
+        # Assign data to 
+
+    # Link and update
+    for ob in obs:
+        sce.objects.link(ob)
+    sce.update()
+
+
 def draw(cylinder):
     """Add the given cylinder to the scene.
 
@@ -89,25 +149,33 @@ def main(args):
     )
     print("Drawing the cylinders.")
     cylinders = graphics.draw(lstring)
-    print("Saving", len(cylinders), "cylinders to '" + basename + "-cylinders.json'")
-    graphics.dump(cylinders, basename + "-cylinders")
 
-    print("Adding cylinders to Blender scene.")
+    cylinder_count = 0
+    for c in cylinders:
+        cylinder_count += len(next(iter(c.values())))
+
+    # print("Saving", len(cylinders), "cylinders to '" + basename + "-cylinders.json'")
+    # graphics.dump(cylinders, basename + "-cylinders")
+    print("Adding {} cylinders to Blender scene.".format(cylinder_count))
+
     # TODO: It's possible to combine objects from multiple blender files. Split up cylinders on large fractals.
     bpy.ops.wm.read_factory_settings(use_empty=True)
-    for i, cylinder in enumerate(cylinders, start=1):
-        print("\rprogress: {}%".format(100 * i // len(cylinders)), end="")
-        draw(cylinder)
 
-        if i % 20 == 0:
-            bpy.ops.object.select_all(action='DESELECT')
-            bpy.ops.object.select_by_type(type='MESH')
-            bpy.ops.object.join()
+    draw_all(cylinders)
 
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.ops.object.select_by_type(type='MESH')
-    bpy.ops.object.join()
-    bpy.ops.object.shade_smooth()
+    # for i, cylinder in enumerate(cylinders, start=1):
+    #     print("\rprogress: {}%".format(100 * i // len(cylinders)), end="")
+    #     draw(cylinder)
+
+    #     if i % 20 == 0:
+    #         bpy.ops.object.select_all(action='DESELECT')
+    #         bpy.ops.object.select_by_type(type='MESH')
+    #         bpy.ops.object.join()
+
+    # bpy.ops.object.select_all(action='DESELECT')
+    # bpy.ops.object.select_by_type(type='MESH')
+    # bpy.ops.object.join()
+    # bpy.ops.object.shade_smooth()
 
     print("\nSaving scene to '" + basename + ".blend'")
     bpy.ops.wm.save_mainfile(filepath=basename + ".blend")
