be4fb0bbaa07ea9b004778ed750f1d09ae1f5f48
Austin Gill <notgnoshi@gmail.com>
2019-04-10 17:53:28 -0600

    Inject better colors and figure settings for matplotlib

--- natural/__init__.py
+++ natural/__init__.py
@@ -1,1 +1,13 @@
 """Python module implementing fractals and cellular automata."""
+import matplotlib as mpl
+
+# Apparently, SNS stands for "Samuel Norman Seaborn", a fictional
+# character from The West Wing
+import seaborn as sns
+
+sns.set()
+# Increase default figure size and DPI
+scale = 1.8
+mpl.rcParams["figure.figsize"] = (scale * 6, scale * 4)
+mpl.rcParams["figure.dpi"] = 300
+mpl.rcParams["savefig.bbox"] = "tight"
