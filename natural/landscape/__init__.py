95e1b74978957a515719af1451b908340f308dfa
Austin Gill <notgnoshi@gmail.com>
2019-04-10 21:15:59 -0600

    Add 2D plotting

--- natural/landscape/__init__.py
+++ natural/landscape/__init__.py
@@ -1,3 +1,3 @@
 """Create random fractal landscapes."""
-from .random_displacement import rand_displacement_1d
-from .plotting import plot_displacement_1d
+from .random_displacement import rand_displacement_1d, rand_displacement_2d
+from .plotting import plot_displacement_1d, plot_displacement_2d
--- natural/landscape/plotting.py
+++ natural/landscape/plotting.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def __check_1d_args(xs, label, filename):
+def __check_1d_args(xs, label):
     if not isinstance(xs, (np.ndarray, tuple)):
         raise ValueError("'xs' must be an np.ndarray or tuple of np.ndarrays")
 
@@ -24,18 +25,19 @@ def __check_1d_args(xs, label, filename):
 
 
 # TODO: Pass in the title?
-def plot_displacement_1d(xs, labels=None, filename=None, gui=False):
+def plot_displacement_1d(xs, labels=None, title=None, filename=None, gui=False):
     """Plot the given 1D fractal heightmap(s).
 
     :param xs: The heightmap or heightmaps to plot
-    :param xs: Either an np.ndarray or tuple of np.ndarrays
+    :type xs: Either an np.ndarray or tuple of np.ndarrays
     :param labels: The label or labels for each of the heightmaps, defaults to None
-    :param labels: A string or tuple of strings, optional
+    :type labels: A string or tuple of strings, optional
     :param filename: The filename to save the plot to, defaults to None
+    :param gui: Whether to open a GUI window with the plot, defaults to False
     """
     plt.clf()
 
-    single = __check_1d_args(xs, labels, filename)
+    single = __check_1d_args(xs, labels)
     if single:
         xs = (xs,)
         _labels = (labels,)
@@ -50,12 +52,66 @@ def plot_displacement_1d(xs, labels=None, filename=None, gui=False):
     for heightmap, label in zip(xs, _labels):
         plt.plot(np.linspace(0, width, len(heightmap)), heightmap, label=label)
 
-    plt.title("1D Random Midpoint Displacement")
+    if title is None:
+        plt.title("1D Random Midpoint Displacement")
+    else:
+        plt.title(title)
     plt.xlabel("$x$")
     plt.ylabel("$h$")
 
     if labels is not None:
         plt.legend()
+
+    if filename is not None:
+        plt.savefig(filename)
+
+    if gui:
+        plt.show()
+
+
+def plot_displacement_2d(Z, title=None, filename=None, gui=False):
+    """Plot the given 2D fractal heightmap.
+
+    :param Z: The 2D heightmap to plot
+    :param filename: The filename to save the plot to, defaults to None
+    :param gui: Whether to open a GUI window with the plot, defaults to False
+    """
+    plt.clf()
+
+    nx, ny = Z.shape
+    X, Y = np.meshgrid(np.linspace(0, 10, nx), np.linspace(0, 10, ny))
+    floor = Z.min()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    # ax.view_init(axim=, elev=)
+
+    ax.plot_surface(
+        X,
+        Y,
+        Z,
+        linewidth=0.1,
+        antialiased=False,
+        cmap=sns.cubehelix_palette(reverse=True, dark=0.1, as_cmap=True),
+    )
+    ax.contour(
+        X,
+        Y,
+        Z,
+        20,
+        zdir="z",
+        offset=floor - 2.5,
+        cmap=sns.cubehelix_palette(reverse=True, dark=0.1, as_cmap=True),
+    )
+    ax.set_zlim(floor - 2.5, Z.max())
+
+    if title is None:
+        plt.title("2D Random Midpoint Displacement")
+    else:
+        plt.title(title)
+    ax.set_xlabel("$x$")
+    ax.set_ylabel("$y$")
+    ax.set_zlabel("$h$")
 
     if filename is not None:
         plt.savefig(filename)
