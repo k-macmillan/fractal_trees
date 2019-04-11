befbd4f03c075f08b6082626765e5c07d88cd83f
Austin Gill <notgnoshi@gmail.com>
2019-04-11 15:37:58 -0600

    Fix blender import bug.

--- natural/__init__.py
+++ natural/__init__.py
@@ -1,13 +1,17 @@
 """Python module implementing fractals and cellular automata."""
-import matplotlib as mpl
+# Only inject custom drawing parameters if Blender failed to import.
+try:
+    import bpy
+except ImportError:
+    import matplotlib as mpl
 
-# Apparently, SNS stands for "Samuel Norman Seaborn", a fictional
-# character from The West Wing
-import seaborn as sns
+    # Apparently, SNS stands for "Samuel Norman Seaborn", a fictional
+    # character from The West Wing
+    import seaborn as sns
 
-sns.set()
-# Increase default figure size and DPI
-scale = 1.8
-mpl.rcParams["figure.figsize"] = (scale * 6, scale * 4)
-mpl.rcParams["figure.dpi"] = 300
-mpl.rcParams["savefig.bbox"] = "tight"
+    sns.set()
+    # Increase default figure size and DPI
+    scale = 1.8
+    mpl.rcParams["figure.figsize"] = (scale * 6, scale * 4)
+    mpl.rcParams["figure.dpi"] = 300
+    mpl.rcParams["savefig.bbox"] = "tight"
